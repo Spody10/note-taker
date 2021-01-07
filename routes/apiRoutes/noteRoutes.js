5acceb683fe8fe527679bd3fffe13477db1c1446
Michael <michaelpspore@gmail.com>
2021-01-07 14:29:01 -0600

    post routes setup

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
+const { addNewNote, validateNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes)
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    }
+    else {
+        const note = addNewNote(req.body, notes);
+        res.json(req.body);
+    }
 });
 
 module.exports = router;
